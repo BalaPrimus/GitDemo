569b75f7d0fe32c238921d29e889770a53b2a5a9
TL <TL@gmail.com>
2021-07-24 18:45:56 +0530

    Created Test.java by the TL.

---
+++ TestingDemo/src/com/gaga/test/Test.java
@@ -0,0 +1,12 @@
+package com.gaga.test;
+
+import com.gaga.beans.Utility;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println(u.getSystemDate());
+	}
+
+}
